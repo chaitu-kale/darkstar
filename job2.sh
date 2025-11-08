3a476cf96d2b221efad5e1fc6a334a6ff9dab310
chaitu-kale <chaitanykale1689@gmail.com>
2025-11-08 15:13:33 +0530

    q

--- job2.sh
+++ job2.sh
@@ -1,4 +1,4 @@
 docker stop darkstar
 docker rm darkstar
-docker pull ishanj10/darkstar
-docker run -d -p 3000:3000 --name=darkstar darkstar:latest 
+docker pull ishanj10/darkstar:1.0
+docker run -d -p 3000:3000 --name=darkstar ishanj10/darkstar:1.0 
